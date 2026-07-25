3176df9b8120cb9ad734d22f626a60a1a62cef1b
MIsmaeelQazi <ismaeelqazi30@gmail.com>
2026-07-25 14:38:04 +0000

    yap added

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return send_from_directory(os.getcwd(), "OneKeyReaction.html")
+    return send_from_directory(os.getcwd(), "index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
